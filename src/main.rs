d490a328265f0d5a74827b5bf39bb0f6fb92b560
smotouchi <shuya-motouchi@gmo.jp>
2017-10-11 10:27:03 +0900

    server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
+    loop {
+        let (stream, peer_addr) = listener.accept().unwrap();
+        println!("{}", peer_addr);
+    }
 }
