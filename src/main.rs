2799ff0017ae735fe7b9a8f04eac8d97c8ddd568
smotouchi <shuya-motouchi@gmo.jp>
2017-10-12 18:41:12 +0900

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::net::TcpListener;
+use std::process::Command;
 
 fn main() {
-    let listener = TcpListener::bind("0.0.0.0:3333").unwrap();
-    loop {
-        let (stream, peer_addr) = listener.accept().unwrap();
-        println!("{}", peer_addr);
+    let status = Command::new("ping").arg("-c 3").arg("8.8.8.8").status().unwrap_or_else(|e| {
+        panic!("failed to execute process: {}", e)
+    });
+    println!("process exited with: {}", status);
     }
-}
+
+fn get_addr() {
+    }
